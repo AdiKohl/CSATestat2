cb5940e6181b9a830d19ee720c8559dec13c49e1
AdiKohl <adrian.kohler@stud.hslu.ch>
2018-05-22 18:36:24 +0200

    asdasdasd

--- FahrbefehlServer/Program.cs
+++ FahrbefehlServer/Program.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
 namespace FileServer
+
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C# on Windows Embedded Systems");
+            
+            TcpListener listen = new TcpListener(IPAddress.Any, 8888);
+            // 4.
+            //TcpListener listen = new TcpListener(IPAddress.Any,13);
+            listen.Start();
+            while (true)
+            {
+                Console.WriteLine("Warte auf Verbindung auf Port " +
+                    listen.LocalEndpoint + "...");
+                TcpClient client = listen.AcceptTcpClient();
+                Console.WriteLine("Verbindung zu " +
+                    client.Client.RemoteEndPoint);
+                TextWriter tw = new StreamWriter(client.GetStream());
+                tw.Write(DateTime.Now.ToString());
+                tw.Flush();
+                client.Close();
+            }
         }
     }
 }
