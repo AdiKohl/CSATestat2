93d9b00829b16865cc4cb9557b198803e6cee41c
Urs Mueller <urs.mueller@gmx.ch>
2018-05-22 20:23:28 +0200

    File HTTP Server komplett
    ausgetestet funktioniert

--- HTTPServer/HTTPHandler.cs
+++ HTTPServer/HTTPHandler.cs
@@ -19,36 +19,48 @@ namespace HTTPServer
         {
             try
             {
-                string htmlText = "noch keine Daten vorhanden!"; //Rohtext wird später überschrieben
+                string data = "noch keine Daten vorhanden!"; //Rohtext wird später überschrieben
                 int contentLength = 0; //länge des Datenstrings der Datei ToDo
 
                 StreamReader sr = new StreamReader(myClient.GetStream());
                 StreamWriter sw = new StreamWriter(myClient.GetStream());
                 Console.WriteLine("Verbindung zu " +
                     myClient.Client.RemoteEndPoint);
-                // Datei lesen
+
+                // Datei lesen wenn Browserverbindung steht
+                string header = sr.ReadLine();
+                if (header.Contains("GET"))
+                {
+                    try
+                    {
+                        using (StreamReader srf = new StreamReader(@"Temp\DataFile.txt"))
+                        {
+                            data = srf.ReadToEnd();
+                            contentLength = data.Length;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Da war ein Error: " + e.ToString());
+                    }
+                }
 
 
                 // Datei im HTTP-Format senden
+                //Header
                 sw.WriteLine("HTTP/1.1 200 OK");
                 sw.WriteLine("Server: RoboServer");
-                sw.WriteLine("Content - type: text / plane");
-                sw.WriteLine("Content - length: "+ contentLength);
+                sw.WriteLine("Content-type: text/plane");
+                sw.WriteLine("Content-length: " + contentLength);
+
                 sw.WriteLine(""); //Leerzeile trennt Header von Body
 
-                //HTML:
-                sw.WriteLine("<html>");
-                sw.WriteLine("<head>");
-                sw.WriteLine("<title>Gruppe Mueller Kohler</title>");
-                sw.WriteLine("</head>");
-                sw.WriteLine("<body>");
-                sw.WriteLine("<div>");
-                sw.WriteLine("<h1>Urs Mueller, Adrian Kohler</h1");
-                sw.WriteLine(htmlText);
-                sw.WriteLine("</div>");
-                sw.WriteLine("</body>");
-                sw.WriteLine("</html>");
-                //Ende HTML
+                //Body
+                sw.WriteLine("Data-File von Urs Mueller, Adrian Kohler");
+                sw.WriteLine(" ");
+                sw.WriteLine(data); //Daten senden
+
+                //Ende HTTP
 
                 sw.Flush(); //ohne Flush wird nix angezeigt
 
--- HTTPServer/Program.cs
+++ HTTPServer/Program.cs
@@ -11,11 +11,23 @@ namespace HTTPServer
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             HTTPServer hs = new HTTPServer(8080);
             new Thread(hs.Start).Start();
-            
+
+            //only for debuging:
+            //if (File.Exists(@"Temp\DataFile.txt")) {
+            //    File.Delete(@"Temp\DataFile.txt");
+            //}
+            //using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"Temp\TestFile.txt", true))
+            //{
+            //    file.WriteLine("Test File ");
+            //    file.WriteLine(" ");
+            //    file.WriteLine("hier wuerde sinnvolles stehen ");
+            //}
+
         }
     }
 }
